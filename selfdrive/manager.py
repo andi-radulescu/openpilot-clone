86a4a785f4ac64bd05627c7894bb83db2e1db58c
Vehicle Researcher <user@comma.ai>
2020-02-06 13:51:42 -0800

    openpilot v0.7.2 release

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1575f51413c19697ba7fbdaaca0b65fc0bb396d96bfa4b987f676e072b8c1686
-size 15346
+oid sha256:00591086aec962ce7389fe4baedf30aaa4211e1125bb124e0eeefbffd4a717b3
+size 15653
--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c725d32f851ff891c567c8b7546f8cb6ea4a248500af38edd3234567e184b451
-size 34471
+oid sha256:828e01d4229f83f3d41629fdb68d8e2e4490d50832f464fca93a6c83b7ae1ef4
+size 34275
