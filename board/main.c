9b4588a0b8feecb8db1493c13b14a57f2b74c0c7
Vehicle Researcher <user@comma.ai>
2020-02-06 13:51:37 -0800

    Squashed 'panda/' changes from 3b356216..73a60d5c
    73a60d5c uds: clear rx buffer on drain
    3b20804b uds: rx message buffering
    d034f3e9 Added the option to turn on IR in power savings mode (#431)
    82cafd10 Allow fan control in power saving (#429)
    186d9dcb Fixed GMLAN interrupts when used in Tesla safety (#428)
    f67ec28e Fix msg checks for non Honda and Toyota (#426)
    
    git-subtree-dir: panda
    git-subtree-split: 73a60d5c9c358e9d5c24aff2f605c3ab650fc74d

--- board/main.c
+++ board/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ff4b4061c6e9bd011dd9785192e1137510004f9cfda90eb957baa808ac53049
-size 24762
+oid sha256:015fa4ecf2cb23986dd37c257458af7f5461fd3fc1b6fc3ba864e4a4a3dec636
+size 24600
