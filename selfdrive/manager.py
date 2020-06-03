ed24f1ecfcf352a0dfe0c0ba9bb1d959c6d4a253
Willem Melching <willem.melching@gmail.com>
2020-06-02 17:39:58 -0700

    run deleter when offroad

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33039eb4ae1a3dfd1373193fe155e6a88c8dbc4b1e0791be4a1abc0c3927e938
+oid sha256:dd97b223cb0eee8d887c32ff0ef5f910615005c282d863766616bf996d97a0cb
 size 17800
